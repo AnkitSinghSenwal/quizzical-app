e0e5c9be7262b879ae9100cf53b9d44b8988ea5d
AnkitSinghSenwal <ankitsinghsenwal2001@gmail.com>
2024-10-21 21:53:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./component/Home/Home"
+import Quiz from "./component/Quiz/Quiz"
+import {useState} from 'react'
+function App(){
+
+  const [page, setPage] = useState(false);
+  
+  function changePage(){
+    setPage(prevPage => !prevPage);
+  }
+
+
+  return( page ? <Quiz/> : <Home redirect ={()=> changePage()}/> );
+}
+
+
+export default App
